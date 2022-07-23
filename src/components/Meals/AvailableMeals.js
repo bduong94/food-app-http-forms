49bed47dff9d87f6ce967252f5c34bc12d6e0ecf
Brian Duong <brianminhduong@gmail.com>
2022-07-23 12:48:08 -0400

    Add state for httpError

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -7,12 +7,18 @@ import classes from "./AvailableMeals.module.css";
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const fetchMeals = async () => {
       const response = await fetch(
-        "https://react-http-6e23b-default-rtdb.firebaseio.com/meals.json"
+        "https://react-http-6e23b-default-rtdb.firebaseio.com/meals.js"
       );
+
+      console.log(response.ok);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
 
       const responseData = await response.json();
       const loadedData = [];
@@ -29,13 +35,26 @@ const AvailableMeals = () => {
       setIsLoading(false);
     };
 
-    fetchMeals();
+    try {
+      fetchMeals();
+    } catch (error) {
+      setIsLoading(false);
+      setHttpError(error.message);
+    }
   }, []);
 
   if (isLoading) {
     return (
       <section className={classes.MealsLoading}>
         <p>...is Loading</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
       </section>
     );
   }
